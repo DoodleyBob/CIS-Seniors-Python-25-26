5ba6c2a44b9d1d6224fd683d8b12748e964273d6
ManI'mDead <cte-elijah.snyder@sllboces.org>
2025-09-17 14:20:05 +0000

    Wednesday 9/17

---
+++ variables.py
@@ -0,0 +1,37 @@
+'''
+File name: variables
+Name: Elijah Snyder
+Date 09/17/2025
+
+Practicing variables basic
+'''
+
+#Variables can hold all data types
+myInt = 42
+myFloat = 3.1415
+myString = "Elijah"
+myBool = True
+
+print("My name is:", myString)
+
+myAge = myInt
+
+print("My age is:", myAge)
+
+print(myFloat)      #3.1415
+myFloat = 6.28210
+print(myFloat)      #6.28210
+
+print("\n\n\n")
+print("My Ticket App")
+print("----------------")
+
+ticketTotal = 43.50 + 43.50 + 43.50
+processFee = 2.95
+venueFee = 3.95 + 3.95 + 3.95
+
+print("\nMy Subtotal is:")
+print("--------------------")
+subTotal = ticketTotal + processFee + venueFee
+print("$", subTotal)
+
