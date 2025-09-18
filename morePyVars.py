d8c9a773a8d25975c2af98136ed4d4362fb333fd
ManI'mDead <cte-elijah.snyder@sllboces.org>
2025-09-18 13:17:49 +0000

    Thursday 9/18

---
+++ morePyVars.py
@@ -0,0 +1,34 @@
+'''
+The Doc String
+Description: Notes about Python Variables
+Name: Elijah Snyder
+Date: 09/18/2025
+'''
+
+#Variable Assignment
+firstName = "Elijah"
+lastName = "Snyder"
+gradeLvl = 12
+favoriteSubject = "Computer Science"
+yearsInSchool = "allat"
+
+print("My name is " + firstName + " " + lastName + ".")
+print("I am in " , gradeLvl , "th grade.")
+print("My favorite subject is " + favoriteSubject + ".")
+print("I have been in school for " + yearsInSchool + " of years.")
+
+print("\n\n\n")
+
+print("Mathematical Calculations:")
+
+#Storing Mathematical values
+length = 15
+width = 8
+height = 12
+
+#Calculate area and volume and store them in variables
+area = length * width
+volume = length * width * height
+
+# Print out the values
+print("The area of our structure is " , area , ", and the volume of our structure is " , volume , ".")
