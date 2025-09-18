46bc018d9ada1b75a4532934ac526c07b10946b8
ManI'mDead <cte-elijah.snyder@sllboces.org>
2025-09-18 14:38:52 +0000

    Thursday 9/18 part 4

---
+++ Unit 1: Computational Thinking/socialProfile.py
@@ -0,0 +1,32 @@
+'''
+Doc String Data
+File Name: socialProfile.py
+Name: Elijah Snyder
+Course Name: Computer & Info Science
+Date 9/18/2025
+'''
+
+username = "Elijah04"
+bio = "I like food."
+favorite_quote = "Long before time had a name, the First Spinjitzu Master created Ninjago using four elemental weapons. But when he passed, a dark presence, Lord Garmadon, sought out to find them. So I, Sensei Wu, have sought to find four ninja to do so before him. - Master Wu"
+
+followers = 2
+following = 174
+posts = 7
+
+average_likes_per_post = 4.6
+engagement_rate = 0.3
+
+verified_account = False
+private_profile = False
+
+print("Username: " + username)
+print("Bio: " + bio)
+print("Favorite Quote: " + favorite_quote)
+print("Followers: " , followers)
+print("Following: " , following)
+print("Posts: " , posts)
+print("Average Likes: " , average_likes_per_post)
+print("Engagement Rate: " , engagement_rate , "%")
+print("Verified Account: ", verified_account)
+print("Private Profile: " , private_profile)
