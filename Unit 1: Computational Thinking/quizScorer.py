4c50880e98bf68e868292e2dc809cd251bbab44e
ManI'mDead <cte-elijah.snyder@sllboces.org>
2025-09-26 14:05:42 +0000

    Friday: 09/26/2025

--- Unit 1: Computational Thinking/quizScorer.py
+++ Unit 1: Computational Thinking/quizScorer.py
@@ -10,28 +10,27 @@ test = int(input("How many questions are on the quiz? (5-10) "))
 print("Scoring quiz for " + name + " has ", test , "questions.")
 
 print("Enter student's aswers (1 for true, 0 for false")
-for question in range(test):
+totalCorrect = 0
+for question in range(int(test)):
     answer = int(input("Question " + str(question + 1) + ": "))
     if answer == 0:
         print("Incorrect ")
     elif answer == 1:
         print("Correct")
+        totalCorrect += 1
     else:
         print("Invalid input, please enter either a 1 or a 0! ")
 
+print("\n")
+print("=" * 25)
+print("QUIZ RESULTS FOR ", name)
+print("=" * 25)
+print("Correct Answers:", totalCorrect, "/",test)
+quizPercentage = float((totalCorrect / test) * 100)
+print("Quiz Percentage: ",quizPercentage,"%")
+if quizPercentage < 70.0:
+    print("Final Status: FAIL :(")
+elif quizPercentage >= 70.0:
+    print("Final Status: PASSED :D")
 
-totalCorrect = 0
-
-print("\n\n")
-print("=" * 30)
-print("Quiz results: ")
-print("=" * 30)
-
-score = totalCorrect / int(test) * 100
-print(name + " got ", totalCorrect , "questions correct.")
-print(name + " got a score of ", score ,"%")
-
-if score >= 70:
-    print("You passed the quiz!")
-elif score < 70:
-    print("You failed your quiz :(")
+print("=" * 25)
