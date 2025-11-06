ec88ca9eed11cc56501065080494420c7e249087
ManI'mDead <cte-elijah.snyder@sllboces.org>
2025-11-06 15:04:26 +0000

    Thursday: 11/06/2025 - 2

--- unit3Types/gradeCalc.py
+++ unit3Types/gradeCalc.py
@@ -4,7 +4,7 @@ exam3_grade = float(input("Enter score on Exam 3 (out of 100): "))
 
 examAvg = (exam1_grade + exam2_grade + exam3_grade) / 3
 
-print(f"Your overall grade is: {examAvg:.2f}")
+
 
 print("\n")
 
@@ -27,7 +27,7 @@ grade4 = calculate_grade(program4)
 
 # Calculate the overall average
 progAvg = ((grade1 + grade2 + grade3 + grade4) / 4) * 100
-print(f"\nStudent overall average on programming assignments: {progAvg:.2f}")
+
 
 # Calculates the overall grade for four equally weighted programming assignments, in which assignments 1 and 2 are graded out of 50 points and assignments 3 and 4 are graded out of 75 points.
 assign1 = int(input("Total number of points for assignment 1:"))
@@ -42,12 +42,15 @@ assignGrade2 = calculate_grade(assign2)
 assignGrade3 = calculate_grade(assign3)
 assignGrade4 = calculate_grade(assign4)
 
-progAvg = ((assignGrade1 + assignGrade2 + assignGrade3 + assignGrade4) / 4) * 100
-print(f"\nStudent overall average on programming assignments: {progAvg:.2f}")
+assignAvg = ((assignGrade1 + assignGrade2 + assignGrade3 + assignGrade4) / 4) * 100
+
 
 
 # Calculates the overall grade for a course with three equally weighted exams (graded out of 100 points) that account for 60% of the overall grade and four equally weighted programming assignments (graded out of 50 points) that account for 40% of the overall grade. Hint: The overall grade can be calculated as 0.6 * averageExamScore + 0.4 * averageProgScore.
 print("\n\n Section 3 of the assignment:\n")
 
-overallAvg = (examAvg * 0.6) + (progAvg * 0.4)
-print(f"The students overall average is: {overallAvg:.2f}")
+overallAvg = (examAvg * 0.5) + (progAvg * 0.4) + (assignAvg * 0.1)
+print(f"Your exam average: {examAvg:.2f}")
+print(f"Student's programming average on programming assignments: {progAvg:.2f}")
+print(f"Students assignment average: {assignAvg:.2f}")
+print(f"The students overall average: {overallAvg:.2f}")
